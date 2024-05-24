70dd18d7064e2dd77d7f9c203bdf9f2abe8d064a
Fempty34 <fempty@protonmail.com>
2024-05-24 11:35:15 +0300

    Make D

--- TinkoffGeneration/DFS/D.cpp
+++ TinkoffGeneration/DFS/D.cpp
@@ -1,5 +1,5 @@
-//#pragma GCC optimize("O3,unroll-loops")
-//#pragma GCC target("avx2")
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2")
 
 #include <bits/stdc++.h>
 #include <cmath>
@@ -11,22 +11,62 @@ typedef string str;
 
 #define all(a) a.begin(), a.end()
 #define int long long
-#define print_array(_v) for(int i=0;i<(_v).size();++i){cout<<(_v)[i]<<' ';}cout<<endl;
-#define scan_array(_v) for(int i=0;i<(_v).size();i++){cin >> (_v)[i];}
-#define fr first 
-#define sc second 
+// #define print_array(_v) for(int i=0;i<(_v).size();++i){cout<<(_v)[i]<<' ';}cout<<endl;
+// #define scan_array(_v) for(int i=0;i<(_v).size();i++){cin >> (_v)[i];}
+#define fr first
+#define sc second
+#define pb push_back
 
-int INF = 1e9 + 1;
+int INF = 1e18 + 1;
 const int MOD = 1e9;
-//const int N = 1e5 + 19;
+const int N = 1e5 + 19;
 
-    
+vector<pair<int, int>> g[N];
+vector<int> order;
+
+int used[N];
+
+void dfs(int v) {
+    used[v] = 1;
+    for (auto u : g[v]) {
+        if (!used[u.fr]) {
+            dfs(u.fr);
+        }
+    }
+    order.pb(v);
+}
+
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n, m, s, t;
+    cin >> n >> m >> s >> t;
 
-    
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].pb({v, w});
+    }
 
-    
+    for (int i = 1; i <= n; i++) {
+        if (!used[i])
+            dfs(i);
+    }
+    reverse(all(order));
+
+    vector<int> dist(n + 1, INF);
+    dist[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = order[i];
+        for (auto u : g[v]) {
+            dist[u.fr] = min(dist[u.fr], dist[v] + u.sc);
+        }
+    }
+    if (dist[t] >= 1e18 + 1 - 1e17) {
+        cout << "Unreachable";
+    } else {
+        cout << dist[t];
+    }
+
     return 0;
 }
