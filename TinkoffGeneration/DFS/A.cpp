58ab0e26c9fc37558a3fb94369f7ea3b686e7c85
Fempty34 <fempty@protonmail.com>
2024-05-24 10:12:08 +0300

    Make A

--- TinkoffGeneration/DFS/A.cpp
+++ TinkoffGeneration/DFS/A.cpp
@@ -15,17 +15,61 @@ typedef string str;
 #define scan_array(_v) for(int i=0;i<(_v).size();i++){cin >> (_v)[i];}
 #define fr first 
 #define sc second 
+#define pb push_back
 
 int INF = 1e9 + 1;
 const int MOD = 1e9;
-//const int N = 1e5 + 19;
+const int N = 1e5 + 19;
+
+vector<int> g[N];
+vector<int> order;
+int used[N];
+int cycle[N];
+bool flag = false;
+
+void dfs(int v){
+    used[v] = 1;
+    cycle[v] = 1;
+    for(auto u: g[v]){
+        if(!used[u]){
+            if(cycle[u] == 1){flag = true; break;}
+            dfs(u);
+        }
+    }
+    cycle[v] = 0;
+    order.pb(v);
+}
+
+
 
     
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
 
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g[u].pb(v);
+    }
     
+    for(int i = 1; i <= m; i++){
+        if(!used[i]){
+            dfs(i);
+        }
+    }
+    if(flag){
+        cout << -1 << endl;
+    }
+    else{
+        reverse(all(order));
+        for(auto v: order)
+            cout << v << " ";
+        cout << endl;
+    }
     
     
     return 0;
